9c77fbfd7fb23154ebc5d79f15a0f6c801b1ae97
tanmaypuri <52965504+tanmaypuri@users.noreply.github.com>
2020-01-21 19:16:50 +0530

    find second largest element in an unsorted array

---
+++ SecondLargestElement.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class SecondLargestElement {
+
+	public static void main(String[] args) {
+		Scanner s =new Scanner(System.in);
+		System.out.println("enter size of the array: ");
+		int n=s.nextInt();
+		System.out.println("enter elements: ");
+		int ar[]=new int[n];
+		for(int i=0;i<n;i++)
+			ar[i]=s.nextInt();
+		Arrays.sort(ar);
+		int p=0; 
+	    for (int i=0;i<n-1;i++){  
+	    	if (ar[i] != ar[i+1]){  
+	            ar[p++] = ar[i];  
+	        }  	
+	    }  
+	        ar[p++] = ar[n-1];  
+	    System.out.println(ar[p-2]);
+	}
+
+}
